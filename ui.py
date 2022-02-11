89f48dd1db43963301d9bc9d89b3e0571e7f4efa
Hwi Song <songhwee1@naver.com>
2022-02-11 17:18:08 +0900

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,32 +1,47 @@
+
 import sys
-from PyQt5.QtWidgets import QApplication, QHBoxLayout, QMainWindow, QPushButton
-class MyApp(QMainWindow):
+from tkinter import CENTER
+from tkinter.font import BOLD
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
+
+class MyApp(QWidget):
+
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        # 타이틀바 내용 설정
-        self.setWindowTitle('PyQt5')
-        # 실행 위치
-        self.move(300, 300)
-        # 사이즈
-        self.resize(400, 200)
+        txt1 = QLabel("한결컴퓨터학원 출석체크 프로그램")
+        txt1.setAlignment(Qt.AlignCenter)
+        txt1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        btn1 = QPushButton('회원등록', self)
+        btn1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        btn2 = QPushButton('출석체크', self)
+        btn2.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
-        # 버튼 생성
-        btn = QPushButton("버튼1", self)
+        #메인화면
+        txtbox = QHBoxLayout()
+        txtbox.addWidget(txt1)
+        btnbox = QHBoxLayout()
+        btnbox.addWidget(btn1)
+        btnbox.addWidget(btn2)
+        
+        
+        #레이아웃 추가
+        layout = QVBoxLayout()
+        layout.addLayout(txtbox)
+        layout.addLayout(btnbox)
 
-        # 레이아웃 생성
-        layout = QHBoxLayout()
-        # 레이아웃에 버튼 넣기
-        layout.addWidget(btn)
-        # 최상위 UI에 생성한 Layout 넣기
         self.setLayout(layout)
+        
 
-        # 보여주기
+        self.setWindowTitle('QPushButton')
+        self.setGeometry(300, 300, 300, 200)
         self.show()
 
+
 if __name__ == '__main__':
-   app = QApplication(sys.argv)
-   ex = MyApp()
-   sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
