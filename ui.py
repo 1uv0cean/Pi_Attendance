54f0d1b83113243f1b7f6fb1b89750709b839d33
songhwee1 <songhwee1@naver.com>
2022-02-12 16:32:18 +0900

    function complete

--- ui.py
+++ ui.py
@@ -8,55 +8,82 @@ import cv2
 import numpy as np
 #라즈베리 파이의 경우 아래 주석 해제
 #import picamera
-from os import listdir
+from os import listdir, makedirs
 from os.path import isdir, isfile, join
 from datetime import datetime
 from openpyxl import load_workbook
-
 from Detect import face_detector, run, train, face_classifier, trains
-class Register():
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-    def initUI(self):
-        app = QApplication(sys.argv)
-        
-        window=QWidget()    
-
-        txt1 = QLabel("회원 등록")
-        txt1.setAlignment(Qt.AlignCenter)
-        txt1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-
-        #회원등록 버튼
-        btn1 = QPushButton('회원등록')
-        btn1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-       
-
-        #출석체크 버튼
-        btn2 = QPushButton('출석체크')
-        btn2.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-      
-
-        #메인화면
-        txtbox = QHBoxLayout()
-        txtbox.addWidget(txt1)
-        btnbox = QHBoxLayout()
-        btnbox.addWidget(btn1)
-        btnbox.addWidget(btn2)
-        
-        
-        #레이아웃 추가
-        layout = QVBoxLayout()
-        layout.addLayout(txtbox)
-        layout.addLayout(btnbox)
-
-        window.setLayout(layout)
-        window.show()
-        app.exec_()
+from Register import face_extractor, take_pictures
 
 
 class MyApp(QWidget):
     #############################회원가입#############################
+    # 얼굴을 저장할 디렉토리
+    face_dirs = 'faces/'
+    # haarcascade 학습모듈 로드
+    face_classifier = cv2.CascadeClassifier('haarcascades/haarcascade_frontalface_default.xml')
+
+    # 얼굴 검출 함수
+    def face_extractor(img):
+        # 사진을 회색으로 변환 (연산량을 줄일 수 있음)
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        faces = face_classifier.detectMultiScale(gray,1.3,5)
+        # 얼굴이 없으면 넘어감
+        if faces == ():
+            return None
+        # 얼굴이 있으면 얼굴 부위만 이미지로 만들고
+        for(x,y,w,h) in faces:
+            cropped_face = img[y:y+h, x:x+w]
+        # 자른 이미지를 반환
+        return cropped_face
+
+    # 사진 촬영 함수
+    def take_pictures(self,name):
+        # 해당 이름의 폴더가 없다면 생성
+        if not isdir(self.face_dirs+name):
+            makedirs(self.face_dirs+name)
+
+        # 카메라 ON    
+        cap = cv2.VideoCapture(0)
+        count = 0
+
+        while True:
+            # 카메라로 부터 사진 한장 읽어 오기
+            ret, frame = cap.read()
+            # 사진에서 얼굴 검출 , 얼굴이 검출되었다면 
+            if face_extractor(frame) is not None:
+                count+=1
+                # 촬영한 사진을 200 x 200 사이즈로 조정
+                face = cv2.resize(face_extractor(frame),(200,200))
+                # 흑백으로 바꿈
+                face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+                # 200x200 흑백 사진을 faces/얼굴 이름/숫자.jpg 로 저장
+                file_name_path = self.face_dirs + name + '/' + str(count) +'.jpg'
+                #cv2.imwrite(file_name_path,face)
+
+                dst = cv2.flip(face, flipCode=1)
+                #이미지 파일 저장
+                retval, buffer = cv2.imencode('.jpg', dst)
+                if (retval):
+                    with open(file_name_path, 'wb') as f_writer:
+                        f_writer.write(buffer)
+                
+                # 촬영된 사진이 몇번째 사진인지 창 안에 표시
+                cv2.putText(face,str(count),(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                cv2.imshow('Detected Face',face)
+            else:
+                print("Face not Found")
+                pass
+            
+            # 얼굴 사진 30장을 촬영했거나 enter키 누르면 종료
+            if cv2.waitKey(1)==13 or count==30:
+                break
+
+        # 촬영 종료
+        cap.release()
+        # 창 닫음
+        cv2.destroyAllWindows()
+        print('학습 샘플이미지 추출 완료')
 
     #############################출석체크#############################
 
@@ -180,6 +207,7 @@ class MyApp(QWidget):
                         write_ws= load_wb.active
                         write_ws.append([uid,uname,now])
                         load_wb.save("attend.xlsx")
+                        print("출석체크 완료!")
                         break      
                         
                 #얼굴 검출 안됨 
@@ -208,6 +236,15 @@ class MyApp(QWidget):
         txt1.setAlignment(Qt.AlignCenter)
         txt1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
+        txtid = QLabel("ID:")
+
+        self.eid = QLineEdit()
+
+        txtname = QLabel("이름:")
+
+        self.ename = QLineEdit()
+
+
         #회원등록 버튼
         btn1 = QPushButton('회원등록', self)
         btn1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
@@ -221,14 +258,26 @@ class MyApp(QWidget):
         #메인화면
         txtbox = QHBoxLayout()
         txtbox.addWidget(txt1)
-        btnbox = QHBoxLayout()
-        btnbox.addWidget(btn1)
+        idbox = QHBoxLayout()
+        idbox.addWidget(txtid)
+        idbox.addWidget(self.eid)
+        namebox = QHBoxLayout()
+        namebox.addWidget(txtname)
+        namebox.addWidget(self.ename)
+        infobox = QVBoxLayout()
+        infobox.addLayout(idbox)
+        infobox.addLayout(namebox)
+        registerbox = QHBoxLayout()
+        registerbox.addLayout(infobox)
+        registerbox.addWidget(btn1)
+        btnbox = QVBoxLayout()
         btnbox.addWidget(btn2)
         
         
         #레이아웃 추가
         layout = QVBoxLayout()
         layout.addLayout(txtbox)
+        layout.addLayout(registerbox)
         layout.addLayout(btnbox)
 
         self.setLayout(layout)
@@ -239,10 +288,25 @@ class MyApp(QWidget):
         self.show()
 
     def OnBtn1Clicked(self):
-        self.hide()
-        self.Register = Register()
-        self.Register.exec()
-        self.show()
+        id = self.eid.text()
+        print(id)
+        id_auth = True
+        load_wb = load_workbook("user.xlsx")
+        load_ws = load_wb['Sheet1']
+        '''
+        for cell in load_ws['A']:   # A열의 모든 셀을 확인
+            if cell.value == id:
+                id_auth = False
+        if id_auth == False:
+            print("이미 존재하는 아이디입니다.")
+        else:
+        '''
+        name = self.ename.text()
+        take_pictures(id+"_"+name)
+        write_ws= load_wb.active
+        write_ws.append([id,name])
+        load_wb.save("user.xlsx")
+        QMessageBox.about(self,'회원등록','사용자 입력 완료!')
 
     def OnBtn2Clicked(self):
         models = trains()
